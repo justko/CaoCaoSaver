2c2bf5df32caf48e05d19ab8b40ca9e6d8482271
Kaiser <justkoomega@gmail.com>
2023-10-16 03:02:47 +0800

    Add an empty activity for constructing a game

--- app/src/main/java/ko/me/caocaosaver/MainActivity.java
+++ app/src/main/java/ko/me/caocaosaver/MainActivity.java
@@ -2,13 +2,20 @@ package ko.me.caocaosaver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import ko.me.caocaosaver.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+        mBinding.mainConstructButton.setOnClickListener(
+                v -> startActivity(new Intent(MainActivity.this, ConstructActivity.class)));
     }
 }
